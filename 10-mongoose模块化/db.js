22d66a96c9ff78a44c09ee7a5e9bf277a042b9a3
jianhui <1164367974@qq.com>
2024-08-24 09:33:50 +0800

    添加配置项

--- 10-mongoose模块化/db.js
+++ 10-mongoose模块化/db.js
@@ -3,11 +3,14 @@
 // 导入 Mongoose
 const mongoose = require("mongoose");
 
+// 导入配置文件
+const {DBHOST, DBPORT, DBNAME} = require('./config')
+
 
 //连接数据库
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/hello");
+    await mongoose.connect(`mongodb://${DBHOST}:${DBPORT}/${DBNAME}`);
     console.log("数据库连接成功");
   } catch (error) {
     console.log("数据库连接失败：", error);
