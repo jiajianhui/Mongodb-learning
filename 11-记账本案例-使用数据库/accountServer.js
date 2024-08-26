830f66c38e942de848759795952e0cfce581e7e7
jianhui <1164367974@qq.com>
2024-08-26 14:35:19 +0800

    读取数据库，渲染列表

--- 11-记账本案例-使用数据库/accountServer.js
+++ 11-记账本案例-使用数据库/accountServer.js
@@ -5,14 +5,17 @@ const accountModel = require("./accountModel");
 // 查看全部数据
 async function findAllaccounts() {
      try {
-       const data = await accountModel.find();
-       console.log(data);
+       const data = await accountModel.find().sort({time: 1});
+       console.log("读取成功……", data);
+       return data
+       
      } catch (error) {
        console.log("读取失败……", error);
+       return [] //确保返回数据，不然会报错
      }
 }
 
-// 查看全部数据
+// 插入数据
 async function addAcount(account) {
      try {
        const newAccount = new accountModel(account);
--- 11-记账本案例-使用数据库/routes/index.js
+++ 11-记账本案例-使用数据库/routes/index.js
@@ -18,35 +18,51 @@ const moment = require('moment')
 // console.log(moment("2022-03-01").toDate());
 
 // 导入数据库相关方法 
-const {addAcount} = require('../accountServer')
+const { findAllaccounts,addAcount } = require("../accountServer");
 
 // 账单列表
-router.get('/accountList', function(req, res, next) {
-  let accounts = db.get("account").value();
-  // console.log(accounts);
-  res.render("list", { accounts });
+router.get('/accountList', async function(req, res, next) {
+
+  try {
+    const accounts = await findAllaccounts();
+    // console.log(accounts);
+    res.render("list", { accounts: accounts || [], moment: moment });  //将moment对象传过去，将时间格式化
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('服务器错误，请重试！！！')
+  }
+  
 });
+
 // 添加账单
 router.get("/accountList/creatAccount", function (req, res, next) {
   res.render("creat");
 });
 
+
+// ---------------------------------------------------------
+
+
 // 新增账单
-router.post("/accountList", function (req, res, next) {
-  // 打印账单数据
-  console.log(req.body);
+router.post("/accountList", async function (req, res, next) {
+  try {
+    // 准备数据
+    const newAccount = {
+      ...req.body,
+      // 转换时间格式——2024 - 08 - 26 —— moment —— new Date()
+      time: moment(req.body.time).toDate(),
+    };
+    // 插入数据库
+    await addAcount(newAccount);
+    // 动态设置信息
+    res.render("success", { msg: "添加成功", url: "/accountList" });
 
-  // 插入数据库
-  const newAccount = {
-    ...req.body,
-    // 转换时间格式——2024 - 08 - 26 —— moment —— new Date()
-    time: moment(req.body.time).toDate(),
-  };
-  addAcount(newAccount);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("服务器错误，请重试！！！");
+  }
 
-
-  // 动态设置信息
-  res.render("success", { msg: "添加成功", url: "/accountList" });
 });
 
 // 删除账单
